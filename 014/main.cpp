838cf0cbf2248956d9936508765cb5c53438e18e
shunsei <k.shunsei1227@gmail.com>
2025-01-13 14:38:46 +0900

    initial commit

---
+++ 014/main.cpp
@@ -0,0 +1,26 @@
+// #define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+
+#define rep(i, s, n) for (int i = s; i <= (int)(n); i++)
+#define  all(v) v.begin(),  v.end()
+#define rall(v) v.rbegin(), v.rend()
+#define arrout(a, s, n) rep(i, s, n)cout<<a[i]<<" "; cout<<endl;
+#define vecout(v) for(auto elm : (v))cout<<elm<<" "; cout<<endl;
+
+using namespace std;
+
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+typedef vector<i16> vi16;
+typedef vector<i32> vi32;
+typedef vector<i64> vi64;
+
+int main(){
+    cin.tie(nullptr);
+
+    
+
+    return(0);
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# typical90
+
+[競プロ典型90問](https://atcoder.jp/contests/typical90?lang=ja) で解いた問題を置いておくリポジトリ
